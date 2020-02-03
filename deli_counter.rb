c4f231cd4e2b1b48aec8fc6bf3749a039038f1a8
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-02-03 05:54:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,28 @@
-katz_deli =[]
-def line(katz_deli)
-  if katz_deli < 1 
-    puts "The Line is currently empty."
-  else
-    puts "The line has people in it."
-  end
-name = "Ada" , "Grace" , "Kent"
-def take_a_number(katz_deli,name)
-  loop do 
-    katz_deli += l 
-    puts "Welcome, #{name}. You are number #{katz_deli} in line."
+
+def line(katz_deli) 
+  if katz_deli.size < 1 
+    puts "The line is currently empty."
+  else 
+    "The line is currently #{katz_deli.index} "
   end
   
+  def take_a_number(katz_deli,name)
+    katz_deli = []
+    katz_deli << "Ada"
+    katz_deli << "Grace"
+    katz_deli << "Kent"
+    people_in_line = 0 
+    people_in_line += l 
+    loop do 
+    puts "Welcome #{katz_deli}. You are number #{people_in_line} in line."
+  end
+  
+  def now_serving
+    puts "Currently serving #{katz_deli.shift}."
+  end
   
   
+  
+    
+  
+    
